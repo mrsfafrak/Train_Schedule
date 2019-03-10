44a564872c537c45956c3ec7cebe295ae288fd81
Tori <victoria.fafrak@gmail.com>
2019-03-10 12:36:53 -0400

    corrected time math for train arrivals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,9 +17,9 @@ $("#add-train-btn").on("click", function (event) {
   // get values from add train form
   var trainName = $("#train-name-input").val().trim();
   var trainDest = $("#dest-input").val().trim();
-  var trainStartTime = moment($("#start-time-input").val().trim(), "HH:mm").format("X");
+  // var trainStartTime = moment($("#start-time-input").val().trim(), "HH:mm").format("X");
+  var trainStartTime = $("#start-time-input").val().trim();
   var trainFreq = $("#freq-input").val().trim();
-  // console.log(trainName + trainDest + trainStartTime + trainFreq);
 
   // object of new train data
   var newTrain = {
@@ -43,49 +43,32 @@ $("#add-train-btn").on("click", function (event) {
 // when a child is added to firebase, the following happens
 database.ref().on("child_added", function (childSnapshot) {
   // console.log(childSnapshot.val());
-
   var trainName = childSnapshot.val().name;
   var trainDest = childSnapshot.val().dest;
   var trainStartTime = childSnapshot.val().start;
   var trainFreq = childSnapshot.val().freq;
+  var nextArrival;
+  var minutesAway;
+  // ensures the start time is in the past
+  var trainStartTimePast = moment(trainStartTime, "hh:mm").subtract(1, "years");
 
-        // var trainStartTimePretty = moment(trainStartTime, "X").format("HH:mm");
-
-  // current time 
-  var currentTime = moment().format("HH:mm");
-        // var currentTimePretty = moment(currentTime, "X").format("HH:mm");
-  console.log(currentTime);
-        
-
-  // difference in time between first train and current time
-  // use subtract functions from moment.js *********
-  var diff = currentTime - trainStartTime;
-  console.log(diff);
-  
-  // number of trips that can be completed within diff window of time
-  var trainTrip = diff % trainFreq;
-  console.log(trainTrip);
-
-  // trainTrip + 1 = next trip, multiply by train freq to get total time
-  // add to train start time
-  var nextArrival = ((trainTrip + 1) * trainFreq) + trainStartTime;
-  // make it pretty to display
-  var nextArrivalPretty = moment(nextArrival, "X").format("HH:mm a");
-  console.log(nextArrivalPretty);
-
-
-  var minAway = nextArrival - currentTime;
-  var minAwayPretty = moment(minAway, "X").format("mm");
-  console.log(minAwayPretty);
-
+  // difference in minutes between first train and now
+  var diff = moment().diff(moment(trainStartTimePast), "minutes");
+  // 
+  var remainder = diff % trainFreq;
+  // calculate minutes away
+  minutesAway = trainFreq - remainder;
+  // time of day for next arrival
+  var nextTrain = moment().add(minutesAway, "minutes");
+  nextArrival = moment(nextTrain).format("hh:mm");
 
   // Create the new row
   var newRow = $("<tr>").append(
     $("<td>").text(trainName),
     $("<td>").text(trainDest),
     $("<td>").text(trainFreq),
-    $("<td>").text(nextArrivalPretty),
-    $("<td>").text(minAwayPretty)
+    $("<td>").text(nextArrival),
+    $("<td>").text(minutesAway)
   );
   // Append the new row to the table
   $("#train-table > tbody").append(newRow);
